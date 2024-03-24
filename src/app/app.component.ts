4c130bab20f2b27e061edd1912b846093a2d301c
paco7828 <koszegipatrik888@gmail.com>
2024-03-24 22:35:31 +0100

    Finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NotesListComponent } from './notes-list/notes-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NotesListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
